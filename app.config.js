95326ad80c657ae4d827142b4c8271932645e2c7
HaiderNadaf <haidernadaf67@gmail.com>
2026-01-08 12:10:24 +0530

    added app.config.js file for goggle maps

---
+++ app.config.js
@@ -0,0 +1,14 @@
+// app.config.js
+export default ({ config }) => {
+  return {
+    ...config,
+    android: {
+      ...config.android,
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+  };
+};
